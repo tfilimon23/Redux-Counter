808b79b18ae78b01888155b4243569d84402b0f4
Tabita Filimon <tabitafilimon@gmail.com>
2019-03-04 17:24:26 -0500

    counter working

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -13,6 +13,14 @@ class Counter extends Component {
         // increments after waiting for one second
     };
 
+    increment = () => {
+        this.props.increment(this.props.count)
+    }
+
+    decrement = () => {
+        this.props.decrement(this.props.count)
+    }
+
     render() {
         // Fill in the two button onClick methods
         // Upon clicking these buttons, the count
@@ -20,10 +28,10 @@ class Counter extends Component {
         return (
             <p>
                 Clicked: {this.props.count} times
-                <button onClick={() => {/* Fill me in */ }}>
+                <button onClick={this.increment}>
                     +
                 </button>
-                <button onClick={() => {/* Fill me in */ }}>
+                <button onClick={this.decrement}>
                     -
                 </button>
                  {/* Uncomment these button tags if you got
